4317141fcb25c663926eb9a6b86e63f873c8ce38
EdwardMelendezM <106676335+EdwardMelendezM@users.noreply.github.com>
2023-04-25 19:43:35 -0500

    adding tailwind

--- src/components/files/File.tsx
+++ src/components/files/File.tsx
@@ -4,6 +4,9 @@ import UploadFile from "./UploadFile";
 const FileManagement = () => {
   return (
     <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
       <ToasterProvider />
       <UploadFile/>
     </div>
